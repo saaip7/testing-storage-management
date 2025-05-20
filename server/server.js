a9c13f6963739779a6e5ed07cbf2af1373b70294
saaip7 <syaifullahhilmimaarij2003@mail.ugm.ac.id>
2025-05-20 10:01:48 +0700

    fix: cors

--- server/server.js
+++ server/server.js
@@ -11,7 +11,12 @@ const PORT = process.env.PORT || 5000;
 
 connectDB();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:3000',
+    'https://storage-management-server.vercel.app'
+  ]
+}));
 app.use(express.json());
 
 // routes
